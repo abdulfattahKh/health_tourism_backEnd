1437267775cb1a47bbb163cb99c619ded7b2e875
a0952432706 <abdulfattah.khudari@gmail.com>
2019-05-26 20:22:13 +0300

    changing the database config

--- routers/clinicsRouter.js
+++ routers/clinicsRouter.js
@@ -5,8 +5,11 @@ const check_author = require('../middlewares/check_authorization');
 
 
 const clinicsController = require('../controllers/clinicsController');
-
 router.get('/clinicTypes', check_auth, check_author(2), clinicsController.getClinicTypes);
 
+// check for admin authentication
+router.get('/clinicsStatus', check_auth, check_author(1), clinicsController.getClinicsStatus);
 
-router.post('/addClinic', check_auth, check_author(2), clinicsController.postAddClinic);
+
+
+module.exports = router;
