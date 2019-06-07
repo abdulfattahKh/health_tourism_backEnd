bdb8f1c88e2aef756cb7f8c99acc6215602a2cde
a0952432706 <abdulfattah.khudari@gmail.com>
2019-06-07 22:30:25 +0300

    some changes in the role sections

--- controllers/clinicsController.js
+++ controllers/clinicsController.js
@@ -1,70 +1,138 @@
-const clinicModel = require('../models/clinicModel');
+const clinicModel = require("../models/clinicModel");
+
+exports.getAllClinics = (req, res, next) => {
+  clinicModel
+    .getAllClinics()
+    .then(result => {
+      return res.status(200).json({
+        success: true,
+        data: result[0],
+        message: "data was fetched correctly"
+      });
+    })
+    .catch(err => {
+      return res.status(500).json({
+        success: false,
+        message: err
+      });
+    });
+};
+
+exports.getMyClinics = (req, res, next) => {
+  if (!req.params["userId"]) {
+    return res.status(400).json({
+      success: false,
+      message: "bad request"
+    });
+  }
+
+  clinicModel
+    .getMyClinics(req.params["userId"])
+    .then(result => {
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          message: "request Not found"
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: "the request was processed successfully.",
+        data: result[0]
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        message: "Internal error"
+      });
+    });
+};
 
 exports.getClinicTypes = (req, res, next) => {
-    clinicModel.getClinicTypes()
-        .then(result => {
-            if (!result) {
-                return res.status(404).json({
-                    success: false,
-                    message: 'request Not found'
-                });
-            }
-            res.status(200).json({
-                success: true,
-                message: 'the request was processed successfully.',
-                data: result[0]
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                success: false,
-                message: 'Internal error'
-            });
+  clinicModel
+    .getClinicTypes()
+    .then(result => {
+      if (!result) {
+        return res.status(404).json({
+          success: false,
+          message: "request Not found"
         });
+      }
+      res.status(200).json({
+        success: true,
+        message: "the request was processed successfully.",
+        data: result[0]
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        message: "Internal error"
+      });
+    });
 };
 
+exports.getClinicsByStatus = (req, res, next) => {
+  if (!req.query["status"]) {
+    return res.status(400).json({
+      success: false,
+      message: "bad requrest"
+    });
+  }
+  clinicModel
+    .getClinicsByStatus(req.query["status"])
+    .then(result => {
+      return res.json({
+        success: true,
+        message: "it was featchd correctly",
+        data: result[0]
+      });
+    })
+    .catch(err => {
+      return res.status(500).json({
+        success: false,
+        message: err
+      });
+    });
+};
 
 exports.getClinicsStatus = (req, res, next) => {
-
-    clinicModel.getClinicsStatus()
-        .then(result => {
-            if (!result) {
-                res.status(404).json({
-                    success: false,
-                    message: 'Data Not found'
-                });
-            }
-            res.status(200).json({
-                success: true,
-                message: 'the request was processed successfully.',
-                data: result[0]
-            });
-        })
-        .catch(err => {
-            res.status(500).json({
-                success: false,
-                message: 'Internal error server'
-            });
-        })
-
+  clinicModel
+    .getClinicsStatus()
+    .then(result => {
+      if (!result) {
+        res.status(404).json({
+          success: false,
+          message: "Data Not found"
+        });
+      }
+      res.status(200).json({
+        success: true,
+        message: "the request was processed successfully.",
+        data: result[0]
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        success: false,
+        message: "Internal error server"
+      });
+    });
 };
 
-
-
 exports.queryAddClinic = (req, res, next) => {
-    const clinicObj = new clinicModel(req.body);
-    clinicObj.save()
-        .then(result => {
-            if (!result) {
-                return res.status(500).json({
-                    success: false,
-                    message: 'Could not adding clinic, Please try again!!'
-                });
-    
-            }
-            res.status(200).json({
-                success: true,
-                message: 'adding clinic successfully.'
-            });
-        })
+  const clinicObj = new clinicModel(req.body);
+  clinicObj.save().then(result => {
+    if (!result) {
+      return res.status(500).json({
+        success: false,
+        message: "Could not adding clinic, Please try again!!"
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "adding clinic successfully."
+    });
+  });
 };
--- routers/clinicsRouter.js
+++ routers/clinicsRouter.js
@@ -11,12 +11,21 @@ router.get(
   clinicsController.getClinicTypes
 );
 
+router.get("/getAllClinics", clinicsController.getAllClinics);
+
 // check for admin authentication
 router.get(
   "/clinicsStatus",
   check_auth,
   check_author([1]),
   clinicsController.getClinicsStatus
+);
+
+router.get(
+  "/ClinicsByStatus",
+  check_auth,
+  check_author([1]),
+  clinicsController.getClinicsByStatus
 );
 
 router.post(
@@ -26,4 +35,16 @@ router.post(
   clinicsController.queryAddClinic
 );
 
+router.get(
+  "/allClinics",
+  check_auth,
+  check_author([1]),
+  clinicsController.getAllClinics
+);
+router.get(
+  "/myClinics/:userId",
+  check_auth,
+  check_author([1, 2]),
+  clinicsController.getMyClinics
+);
 module.exports = router;
